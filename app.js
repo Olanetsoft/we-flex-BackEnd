23ef276f3c00a3aa33a55560c53363ce05a79a24
olanetsoft <heedris2olubisi@gmail.com>
2020-03-27 13:31:38 +0100

    CORS SETUP FOR ACCESSIBILITY FROM OTHER SERVER

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ const app = express();
 app.use(bodyParser.json()); // used for application/json
 
 
+//Setting CORS for request from any server
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 app.use('/feed/', feedRoutes);
 
 
