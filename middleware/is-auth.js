1e4c9fd97b1ea80513c6fad3f4a473e37d08a769
olanetsoft <heedris2olubisi@gmail.com>
2020-03-30 16:44:29 +0100

    AUTHORIZATION ADDED TO INCLUDE JWT

---
+++ middleware/is-auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    //To confirm if the header exist
+    const authHeader = req.get('Authorization');
+    if (!authHeader) {
+      const error = new Error('Not authenticated.');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    //get the token from the header
+    const token = authHeader.split(' ')[1];
+    let decodedToken;
+    try {
+      decodedToken = jwt.verify(token, 'somesupersecretsecret');
+    } catch (err) {
+      err.statusCode = 500;
+      throw err;
+    }
+    if (!decodedToken) {
+      const error = new Error('Not authenticated.');
+      error.statusCode = 401;
+      throw error;
+    }
+    req.userId = decodedToken.userId;
+    next();
+    
+};
--- routes/feed.js
+++ routes/feed.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 
 //GET /feed /posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 //POST /feed/post
 router.post('/post',
