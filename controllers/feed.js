8181d960f55479d65004287345c3503163c441ba
olanetsoft <heedris2olubisi@gmail.com>
2020-03-30 17:47:05 +0100

    CONVERTED GET POSTS TO ASYNC AWAIT

--- controllers/feed.js
+++ controllers/feed.js
@@ -12,32 +12,26 @@ const User = require('../models/user');
 
 
 //gets all posts
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
     const currentPage = req.query.page || 1;
     const perPage = 2;
-    let totalItems;
-    Post.find()
-        .countDocuments()
-        .then(count => {
-            totalItems = count;
-            return Post.find()
-                .skip((currentPage - 1) * perPage)
-                .limit(perPage);
-        })
-        .then(posts => {
-            res.status(200)
-                .json({
-                    message: 'Fetched Post successfully',
-                    posts: posts,
-                    totalItems: totalItems
-                })
-        })
-        .catch(err => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
-        });
+    try {
+      const totalItems = await Post.find().countDocuments();
+      const posts = await Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+  
+      res.status(200).json({
+        message: 'Fetched posts successfully.',
+        posts: posts,
+        totalItems: totalItems
+      });
+    } catch (error) {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    }
 };
 
 
