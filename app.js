675a49679e6f978e0607640a9dfa90705af316e1
olanetsoft <heedris2olubisi@gmail.com>
2020-04-01 19:26:14 +0100

    UPDATED PORT

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ mongoose
         MONGODB_URI
     )
     .then(result => {
-        const server = app.listen(8080);
+        const server =  app.listen(process.env.PORT || 3000);;
         //Setup/Establish socket.io connection
         const io = require('./socket').init(server);
         //Now lets use it to define event listeners
